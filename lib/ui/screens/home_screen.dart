5b8db38589612168d748572a8e1c358d74271777
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-12-11 20:14:03 +0700

    Menyelesaikan slicing bottom navigation bar

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -8,51 +8,68 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightColor,
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: blueColor,
-        unselectedItemColor: blackColor,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        selectedLabelStyle: blueTextStyle.copyWith(
-          fontSize: 10,
-          fontWeight: medium,
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0.0,
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 6,
+        clipBehavior: Clip.antiAlias,
+        child: BottomNavigationBar(
+          backgroundColor: whiteColor,
+          elevation: 0.0,
+          type: BottomNavigationBarType.values[0],
+          selectedItemColor: lightBlueColor,
+          unselectedItemColor: blackColor,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          selectedLabelStyle: blueTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          unselectedLabelStyle: blackTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic-overview.png',
+                width: 20,
+                color: lightBlueColor,
+              ),
+              label: "Overview",
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic-history.png',
+                width: 20,
+              ),
+              label: "History",
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic-statistic.png',
+                width: 20,
+              ),
+              label: "Statistic",
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic-reward.png',
+                width: 20,
+              ),
+              label: "Reward",
+            ),
+          ],
         ),
-        unselectedLabelStyle: blackTextStyle.copyWith(
-          fontSize: 10,
-          fontWeight: medium,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: blueColor,
+        child: Image.asset(
+          'assets/ic-add-circle.png',
+          width: 24,
         ),
-        items: [
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic-overview.png',
-              width: 20,
-              color: blueColor,
-            ),
-            label: "Overview",
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic-history.png',
-              width: 20,
-            ),
-            label: "History",
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic-statistic.png',
-              width: 20,
-            ),
-            label: "Statistic",
-          ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic-reward.png',
-              width: 20,
-            ),
-            label: "Reward",
-          ),
-        ],
       ),
     );
   }
